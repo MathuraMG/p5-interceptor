4ccc3cdb679f839f7bb3919c5dc58c611ac30ef0
mathuramg <mmg9291@gmail.com>
2016-11-23 11:40:43 -0500

    add more lint rules

--- gridInterceptor/intercept-p5.js
+++ gridInterceptor/intercept-p5.js
@@ -1,3 +1,5 @@
+
+
 funcNames = allData['classitems'].map(function(x) {
   if (x['overloads']) {
     tempParam = x['overloads'][0]['params'];
@@ -16,12 +18,15 @@ funcNames = allData['classitems'].map(function(x) {
 funcNames = funcNames.filter(function(x) {
   var className = x['class'];
   return (x['name'] && x['params'] && (className === 'p5'));
-})
+});
 
 funcNames.forEach(function(x) {
   var document = parent.document;
   var originalFunc = p5.prototype[x.name];
-  var byID = function(id) {var element = document.getElementById(id); return element; };
+  var byID = function(id) {
+    var element = document.getElementById(id);
+    return element;
+  };
   var details = byID('textOutput-content-details');
   var summary = byID('textOutput-content-summary');
   var table = byID('textOutput-content-table');
@@ -36,28 +41,24 @@ funcNames.forEach(function(x) {
       Interceptor.createShadowDOMElement(document);
       Interceptor.setupObject =
       Interceptor.populateObject(x, arguments, Interceptor.setupObject, details, false);
-      Interceptor.populateObjectDetails
-      (Interceptor.setupObject, Interceptor.drawObject, summary, details);
+      Interceptor.populateObjectDetails(Interceptor.setupObject, Interceptor.drawObject, summary, details);
       Interceptor.populateTable(details, Interceptor.setupObject);
     } else if (frameCount % 50 == 0) {
       Interceptor.drawObject =
       Interceptor.populateObject(x, arguments, Interceptor.drawObject, details, true);
       Interceptor.isCleared = false;
-    }
-    //reset some of the variables
-    else if (frameCount % 50 == 1) {
+    } else if (frameCount % 50 == 1) { // reset some of the variables
       if (!Interceptor.isCleared) {
         var cells = document.getElementsByClassName('textOutput-cell-content');
         for (i = 0; i < cells.length; i++) {
           cells[i].innerHTML = '';
         }
-        Interceptor.populateObjectDetails
-        (Interceptor.setupObject, Interceptor.drawObject, summary, details);
-        Interceptor.populateTable
-        (Interceptor.setupObject.objectArray.concat(Interceptor.drawObject.objectArray), document);
+        Interceptor.populateObjectDetails(Interceptor.setupObject, Interceptor.drawObject, summary, details);
+        Interceptor.populateTable(Interceptor.setupObject.objectArray.concat(Interceptor.drawObject.objectArray),
+        document);
       }
       Interceptor.drawObject = Interceptor.clearVariables(Interceptor.drawObject);
     }
     return originalFunc.apply(this, arguments);
-  }
+  };
 });
