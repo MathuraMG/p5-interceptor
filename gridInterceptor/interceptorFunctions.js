b8b6fd33828bb5480944eb0a62982c0e5b02d1b6
mathuramg <mmg9291@gmail.com>
2017-01-09 08:41:58 -0500

    replace for with forEach; move interceptor function to prototype

--- gridInterceptor/interceptorFunctions.js
+++ gridInterceptor/interceptorFunctions.js
@@ -3,139 +3,141 @@ function gridInterceptor() {
   var self = this;
   baseInterceptor.call(self);
   this.noRows = 10,
-  this.noCols = 10,
-  this.coordLoc = {},
+    this.noCols = 10,
+    this.coordLoc = {}
+}
 
-  this.clearVariables = function(object) {
-    object.objectTypeCount = {};
-    object.objectCount = 0;
-    this.isCleared = true;
-    return object;
-  },
+gridInterceptor.prototype = Object.create(baseInterceptor.prototype);
 
-  this.createShadowDOMElement = function(document) {
-    var contentTable = document.getElementById('textOutput-content-table');
-    for (var i = 0; i < this.noRows; i++) {
-      var row = document.createElement('tr');
+gridInterceptor.prototype.clearVariables = function(object) {
+  object.objectTypeCount = {};
+  object.objectCount = 0;
+  this.isCleared = true;
+  return object;
+}
 
-      for (var j = 0; j < this.noCols; j++) {
-        var col = document.createElement('td');
-        col.className = 'textOutput-cell-content';
-        col.innerHTML = 'test';
-        row.appendChild(col);
-      }
-      contentTable.appendChild(row);
+gridInterceptor.prototype.createShadowDOMElement = function(document) {
+  var contentTable = document.getElementById('textOutput-content-table');
+  for (var i = 0; i < this.noRows; i++) {
+    var row = document.createElement('tr');
+
+    for (var j = 0; j < this.noCols; j++) {
+      var col = document.createElement('td');
+      col.className = 'textOutput-cell-content';
+      col.innerHTML = 'test';
+      row.appendChild(col);
     }
-    shadowDOMElement = document.getElementById('textOutput-content');
-  },
-  this.populateObject = function(x, arguments, object, table, isDraw) {
-    objectCount = object.objectCount;
-    objectArray = object.objectArray;
-    objectTypeCount = object.objectTypeCount;
-    if (!isDraw) {
-      // check for special function in setup -> createCanvas
-      if (!x.name.localeCompare('createCanvas')) {
-        this.canvasDetails.width = arguments[0];
-        this.canvasDetails.height = arguments[1];
-      }
-    }
-
-    var entityClass = BaseEntity.entityFor(x.name);
-
-    if (entityClass && !entityClass.isParameter) {
-      objectArray[objectCount] = new entityClass(this,x,arguments,this.canvasDetails.width,this.canvasDetails.height);
-
-        if (objectTypeCount[x.name]) {
-          objectTypeCount[x.name]++;
-        } else {
-          objectTypeCount[x.name] = 1;
-        }
-        objectCount++;
-    }
-    else if(entityClass && entityClass.isParameter) {
-      new entityClass(this,x,arguments,this.canvasDetails.width,this.canvasDetails.height);
-    }
-    return ({
-      objectCount: objectCount,
-      objectArray: objectArray,
-      objectTypeCount: objectTypeCount
-    });
-  },
-
-  this.populateTable = function(objectArray, documentPassed) {
-    if (this.totalCount < 100) {
-      for (var i = 0; i < objectArray.length; i++) {
-        var cellLoc = objectArray[i].coordLoc.locY * this.noRows + objectArray[i].coordLoc.locX;
-        // add link in table
-
-        var cellLink = documentPassed.createElement('a');
-        cellLink.innerHTML += objectArray[i].type;
-        var objectId = '#object' + i;
-        cellLink.setAttribute('href', objectId);
-        documentPassed.getElementsByClassName('textOutput-cell-content')[cellLoc].appendChild(cellLink);
-      }
-    }
-  },
-
-  /* helper function to populate object Details */
-  this.populateObjectDetails = function(object1, object2, elementSummary, elementDetail) {
-    this.prevTotalCount = this.totalCount;
-    this.totalCount = object1.objectCount + object2.objectCount;
-    elementSummary.innerHTML = '';
-    elementDetail.innerHTML = '';
-    elementSummary.innerHTML += this.bgColor + ' canvas is ' + this.canvasDetails.width + ' by '
-    + this.canvasDetails.height + ' of area ' + this.canvasDetails.width * this.canvasDetails.height;
-    if (this.totalCount > 1) {
-      elementSummary.innerHTML += ' Contains ' + this.totalCount + ' objects - ';
-    } else {
-      elementSummary.innerHTML += ' Contains ' + this.totalCount + ' object - ';
-    }
-
-    if (object2.objectCount > 0 || object1.objectCount > 0) {
-      totObjectTypeCount = mergeObjRecursive(object1.objectTypeCount, object2.objectTypeCount);
-      var keys = Object.keys(totObjectTypeCount);
-      for (var i = 0; i < keys.length; i++) {
-        elementSummary.innerHTML += totObjectTypeCount[keys[i]] + ' ' + keys[i] + ' ';
-      }
-
-      var objectList = document.createElement('ul');
-
-      if (this.totalCount < 100) {
-        for (var i = 0; i < object1.objectArray.length; i++) {
-          var objectListItem = document.createElement('li');
-          objectListItem.id = 'object' + i;
-          objectList.appendChild(objectListItem);
-          var objKeys = Object.keys(object1.objectArray[i].getAttributes());
-          for (var j = 0; j < objKeys.length; j++) {
-            if (objKeys[j].localeCompare('coordLoc')) {
-              if (objKeys[j].localeCompare('type')) {
-                objectListItem.innerHTML += objKeys[j] + ' = ' + object1.objectArray[i][objKeys[j]] + ' ';
-              } else {
-                objectListItem.innerHTML += object1.objectArray[i][objKeys[j]] + ' ';
-              }
-            }
-          }
-        }
-        for (var i = 0; i < object2.objectArray.length; i++) {
-
-          var objectListItem = document.createElement('li');
-          objectListItem.id = 'object' + (object1.objectArray.length + i);
-          objectList.appendChild(objectListItem);
-          var objKeys = Object.keys(object2.objectArray[i].getAttributes());
-          for (var j = 0; j < objKeys.length; j++) {
-            if (objKeys[j].localeCompare('coordLoc')) {
-              if (objKeys[j].localeCompare('type')) {
-                objectListItem.innerHTML += objKeys[j] + ' = ' + object2.objectArray[i][objKeys[j]] + ' ';
-              } else {
-                objectListItem.innerHTML += object2.objectArray[i][objKeys[j]] + ' ';
-              }
-            }
-          }
-        }
-        elementDetail.appendChild(objectList);
-      }
+    contentTable.appendChild(row);
+  }
+  shadowDOMElement = document.getElementById('textOutput-content');
+}
+gridInterceptor.prototype.populateObject = function(x, arguments, object, table, isDraw) {
+  objectCount = object.objectCount;
+  objectArray = object.objectArray;
+  objectTypeCount = object.objectTypeCount;
+  if (!isDraw) {
+    // check for special function in setup -> createCanvas
+    if (!x.name.localeCompare('createCanvas')) {
+      this.canvasDetails.width = arguments[0];
+      this.canvasDetails.height = arguments[1];
     }
   }
-};
+
+  var entityClass = BaseEntity.entityFor(x.name);
+
+  if (entityClass && !entityClass.isParameter) {
+    objectArray[objectCount] = new entityClass(this, x, arguments, this.canvasDetails.width, this.canvasDetails.height);
+
+    if (objectTypeCount[x.name]) {
+      objectTypeCount[x.name]++;
+    } else {
+      objectTypeCount[x.name] = 1;
+    }
+    objectCount++;
+  } else if (entityClass && entityClass.isParameter) {
+    new entityClass(this, x, arguments, this.canvasDetails.width, this.canvasDetails.height);
+  }
+  return ({
+    objectCount: objectCount,
+    objectArray: objectArray,
+    objectTypeCount: objectTypeCount
+  });
+}
+
+gridInterceptor.prototype.populateTable = function(objectArray, documentPassed) {
+  if (this.totalCount < 100) {
+    for (var i = 0; i < objectArray.length; i++) {
+      var cellLoc = objectArray[i].coordLoc.locY * this.noRows + objectArray[i].coordLoc.locX;
+      // add link in table
+
+      var cellLink = documentPassed.createElement('a');
+      cellLink.innerHTML += objectArray[i].type;
+      var objectId = '#object' + i;
+      cellLink.setAttribute('href', objectId);
+      documentPassed.getElementsByClassName('textOutput-cell-content')[cellLoc].appendChild(cellLink);
+    }
+  }
+}
+
+/* helper function to populate object Details */
+gridInterceptor.prototype.populateObjectDetails = function(object1, object2, elementSummary, elementDetail) {
+  this.prevTotalCount = this.totalCount;
+  this.totalCount = object1.objectCount + object2.objectCount;
+  elementSummary.innerHTML = '';
+  elementDetail.innerHTML = '';
+  elementSummary.innerHTML += this.bgColor + ' canvas is ' + this.canvasDetails.width + ' by ' +
+    this.canvasDetails.height + ' of area ' + this.canvasDetails.width * this.canvasDetails.height;
+  if (this.totalCount > 1) {
+    elementSummary.innerHTML += ' Contains ' + this.totalCount + ' objects - ';
+  } else {
+    elementSummary.innerHTML += ' Contains ' + this.totalCount + ' object - ';
+  }
+
+  if (object2.objectCount > 0 || object1.objectCount > 0) {
+    totObjectTypeCount = mergeObjRecursive(object1.objectTypeCount, object2.objectTypeCount);
+    var keys = Object.keys(totObjectTypeCount);
+    for (var i = 0; i < keys.length; i++) {
+      elementSummary.innerHTML += totObjectTypeCount[keys[i]] + ' ' + keys[i] + ' ';
+    }
+
+    var objectList = document.createElement('ul');
+
+    if (this.totalCount < 100) {
+      object1.objectArray.forEach(function(objArrayItem){
+        var objectListItem = document.createElement('li');
+        objectListItem.id = 'object' + i;
+        objectList.appendChild(objectListItem);
+        var objKeys = Object.keys(objArrayItem.getAttributes());
+        objKeys.forEach(function(objKeyItem){
+          if (objKeyItem.localeCompare('coordLoc')) {
+            if (objKeyItem.localeCompare('type')) {
+              objectListItem.innerHTML += objKeyItem + ' = ' + objArrayItem[objKeyItem] + ' ';
+            } else {
+              objectListItem.innerHTML += objArrayItem[objKeyItem] + ' ';
+            }
+          }
+        });
+      });
+
+      object2.objectArray.forEach(function(objArrayItem){
+        var objectListItem = document.createElement('li');
+        objectListItem.id = 'object' + (object1.objectArray.length + i);
+        objectList.appendChild(objectListItem);
+        var objKeys = Object.keys(objArrayItem.getAttributes());
+        objKeys.forEach(function(objKeyItem){
+          if (objKeyItem.localeCompare('coordLoc')) {
+            if (objKeyItem.localeCompare('type')) {
+              objectListItem.innerHTML += objKeyItem + ' = ' + objArrayItem[objKeyItem] + ' ';
+            } else {
+              objectListItem.innerHTML += objArrayItem[objKeyItem] + ' ';
+            }
+          }
+        });
+      });
+      elementDetail.appendChild(objectList);
+    }
+  }
+}
+
 
 var Interceptor = new gridInterceptor();
--- textInterceptor/interceptorFunctions.js
+++ textInterceptor/interceptorFunctions.js
@@ -1,136 +1,110 @@
 function textInterceptor() { // eslint-disable-line
   var self = this;
   baseInterceptor.call(self);
+}
+textInterceptor.prototype = Object.create(baseInterceptor.prototype);
 
-  this.clearVariables = function(object) {
-    object.objectTypeCount = {};
-    object.objectCount = 0;
-    this.isCleared = true;
-    return object;
-  },
+textInterceptor.prototype.clearVariables = function(object) {
+  object.objectTypeCount = {};
+  object.objectCount = 0;
+  this.isCleared = true;
+  return object;
+}
 
-  this.populateObject = function(x, arguments, object, table, isDraw) {
-    objectCount = object.objectCount;
-    objectArray = object.objectArray;
-    objectTypeCount = object.objectTypeCount;
-    if (!isDraw) {
-      // check for special function in setup -> createCanvas
-      if (!x.name.localeCompare('createCanvas')) {
-        this.canvasDetails.width = arguments[0];
-        this.canvasDetails.height = arguments[1];
-      }
+textInterceptor.prototype.populateObject = function(x, arguments, object, table, isDraw) {
+  objectCount = object.objectCount;
+  objectArray = object.objectArray;
+  objectTypeCount = object.objectTypeCount;
+  if (!isDraw) {
+    // check for special function in setup -> createCanvas
+    if (!x.name.localeCompare('createCanvas')) {
+      this.canvasDetails.width = arguments[0];
+      this.canvasDetails.height = arguments[1];
     }
+  }
 
-    var entityClass = BaseEntity.entityFor(x.name);
+  var entityClass = BaseEntity.entityFor(x.name);
 
-    if (entityClass && !entityClass.isParameter) {
-      objectArray[objectCount] = new entityClass(this,x,arguments,this.canvasDetails.width,this.canvasDetails.height);
+  if (entityClass && !entityClass.isParameter) {
+    objectArray[objectCount] = new entityClass(this, x, arguments, this.canvasDetails.width, this.canvasDetails.height);
 
-        if (objectTypeCount[x.name]) {
-          objectTypeCount[x.name]++;
-        } else {
-          objectTypeCount[x.name] = 1;
-        }
-        objectCount++;
+    if (objectTypeCount[x.name]) {
+      objectTypeCount[x.name]++;
+    } else {
+      objectTypeCount[x.name] = 1;
     }
-    else if(entityClass && entityClass.isParameter) {
-      new entityClass(this,x,arguments,this.canvasDetails.width,this.canvasDetails.height);
-    }
-    return ({
-      objectCount: objectCount,
-      objectArray: objectArray,
-      objectTypeCount: objectTypeCount
-    });
-  },
+    objectCount++;
+  } else if (entityClass && entityClass.isParameter) {
+    new entityClass(this, x, arguments, this.canvasDetails.width, this.canvasDetails.height);
+  }
+  return ({
+    objectCount: objectCount,
+    objectArray: objectArray,
+    objectTypeCount: objectTypeCount
+  });
+}
 
-  this.populateTable = function(table, objectArray) {
-    if (this.totalCount < 100) {
-      if (this.prevTotalCount > this.totalCount) {
-        for (var j = 0; j < this.totalCount; j++) {
-          var row = table.children[j];
-          var tempCol = row.children.length;
-          var properties = Object.keys(objectArray[j].getAttributes());
+textInterceptor.prototype.populateTable = function(table, objectArray) {
+  if (this.totalCount < 100) {
+    if (this.prevTotalCount > this.totalCount) {
+      for (var j = 0; j < this.totalCount; j++) {
+        var row = table.children[j];
+        var tempCol = row.children.length;
+        var properties = Object.keys(objectArray[j].getAttributes());
 
-          if (tempCol < properties.length) { // ie - there are more cols now
-            for (var i = 0; i < tempCol; i++) {
-              if (properties[i].localeCompare('type')) {
-                row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
-              } else {
-                row.children[i].innerHTML = objectArray[j][properties[i]];
-              }
-            }
-            for (var i = tempCol; i < properties.length; i++) {
-              var col = document.createElement('td');
-              if (properties[i].localeCompare('type')) {
-                col.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
-              } else {
-                col.children[i].innerHTML = objectArray[j][properties[i]];
-              }
-
-              row.appendChild(col);
-            }
-          } else { // ie - there are fewer cols now
-            for (var i = 0; i < properties.length; i++) {
-              if (properties[i].localeCompare('type')) {
-                row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
-              } else {
-                row.children[i].innerHTML = objectArray[j][properties[i]];
-              }
-            }
-            for (var i = properties.length; i < tempCol; i++) {
-              var tempCol = row.children[i];
-              row.removeChild(tempCol);
+        if (tempCol < properties.length) { // ie - there are more cols now
+          for (var i = 0; i < tempCol; i++) {
+            if (properties[i].localeCompare('type')) {
+              row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
+            } else {
+              row.children[i].innerHTML = objectArray[j][properties[i]];
             }
           }
-        }
-        for (var j = this.totalCount; j < this.prevTotalCount; j++) {
-          var tempRow = table.children[j];
-          table.removeChild(tempRow);
-        }
-      } else if (this.prevTotalCount <= this.totalCount) {
-        for (var j = 0; j < this.prevTotalCount; j++) {
-          var row = table.children[j];
-          var tempCol = row.children.length;
-          var properties = Object.keys(objectArray[j].getAttributes());
-
-          if (tempCol < properties.length) { // ie - there are more cols now
-            for (var i = 0; i <= tempCol; i++) {
-              if (properties[i].localeCompare('type')) {
-                row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
-              } else {
-                row.children[i].innerHTML = objectArray[j][properties[i]];
-              }
-            }
-            for (var i = tempCol; i < properties.length; i++) {
-              var col = document.createElement('td');
-
-              if (properties[i].localeCompare('type')) {
-                col.innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
-              } else {
-                col.innerHTML = objectArray[j][properties[i]];
-              }
-              row.appendChild(col);
-            }
-          } else { // ie - there are fewer cols now
-            for (var i = 0; i < properties.length; i++) {
-              if (properties[i].localeCompare('type')) {
-                row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
-              } else {
-                row.children[i].innerHTML = objectArray[j][properties[i]];
-              }
-            }
-            for (var i = properties.length; i < tempCol; i++) {
-              var tempCol = row.children[i];
-              row.removeChild(tempCol);
-            }
-          }
-        }
-        for (var j = this.prevTotalCount; j < this.totalCount; j++) {
-          var row = document.createElement('tr');
-          row.id = 'object' + j;
-          var properties = Object.keys(objectArray[j].getAttributes());
-          for (var i = 0; i < properties.length; i++) {
+          for (var i = tempCol; i < properties.length; i++) {
             var col = document.createElement('td');
+            if (properties[i].localeCompare('type')) {
+              col.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
+            } else {
+              col.children[i].innerHTML = objectArray[j][properties[i]];
+            }
+
+            row.appendChild(col);
+          }
+        } else { // ie - there are fewer cols now
+          for (var i = 0; i < properties.length; i++) {
+            if (properties[i].localeCompare('type')) {
+              row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
+            } else {
+              row.children[i].innerHTML = objectArray[j][properties[i]];
+            }
+          }
+          for (var i = properties.length; i < tempCol; i++) {
+            var tempCol = row.children[i];
+            row.removeChild(tempCol);
+          }
+        }
+      }
+      for (var j = this.totalCount; j < this.prevTotalCount; j++) {
+        var tempRow = table.children[j];
+        table.removeChild(tempRow);
+      }
+    } else if (this.prevTotalCount <= this.totalCount) {
+      for (var j = 0; j < this.prevTotalCount; j++) {
+        var row = table.children[j];
+        var tempCol = row.children.length;
+        var properties = Object.keys(objectArray[j].getAttributes());
+
+        if (tempCol < properties.length) { // ie - there are more cols now
+          for (var i = 0; i <= tempCol; i++) {
+            if (properties[i].localeCompare('type')) {
+              row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
+            } else {
+              row.children[i].innerHTML = objectArray[j][properties[i]];
+            }
+          }
+          for (var i = tempCol; i < properties.length; i++) {
+            var col = document.createElement('td');
+
             if (properties[i].localeCompare('type')) {
               col.innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
             } else {
@@ -138,58 +112,85 @@ function textInterceptor() { // eslint-disable-line
             }
             row.appendChild(col);
           }
-          table.appendChild(row);
+        } else { // ie - there are fewer cols now
+          for (var i = 0; i < properties.length; i++) {
+            if (properties[i].localeCompare('type')) {
+              row.children[i].innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
+            } else {
+              row.children[i].innerHTML = objectArray[j][properties[i]];
+            }
+          }
+          for (var i = properties.length; i < tempCol; i++) {
+            var tempCol = row.children[i];
+            row.removeChild(tempCol);
+          }
         }
       }
-    }
-  },
-
-  this.getSummary = function(object1, object2, element) {
-    this.prevTotalCount = this.totalCount;
-    this.totalCount = object1.objectCount + object2.objectCount;
-    element.innerHTML = '';
-    element.innerHTML += this.bgColor + ' coloured canvas is ' + this.canvasDetails.width + ' by '
-    + this.canvasDetails.height + ' of area ' + this.canvasDetails.width * this.canvasDetails.height;
-    if (this.totalCount > 1) {
-      element.innerHTML += ' Contains ' + this.totalCount + ' objects - ';
-    } else {
-      element.innerHTML += ' Contains ' + this.totalCount + ' object - ';
-    }
-
-    if (object2.objectCount > 0 || object1.objectCount > 0) {
-      totObjectTypeCount = mergeObjRecursive(object1.objectTypeCount, object2.objectTypeCount);
-      var keys = Object.keys(totObjectTypeCount);
-      for (var i = 0; i < keys.length; i++) {
-        element.innerHTML += totObjectTypeCount[keys[i]] + ' ' + keys[i] + ' ';
-      }
-
-      var objectList = document.createElement('ul');
-
-      if (this.totalCount < 100) {
-        for (var i = 0; i < object1.objectArray.length; i++) {
-          var objectListItem = document.createElement('li');
-          objectList.appendChild(objectListItem);
-          var objLink = document.createElement('a');
-          objLink.href = '#object' + i;
-          objLink.innerHTML = object1.objectArray[i]['type'];
-          objectListItem.appendChild(objLink);
-          objectListItem.innerHTML += '; area = ' + object1.objectArray[i]['area']
-          + '; location = ' + object1.objectArray[i]['location'];
+      for (var j = this.prevTotalCount; j < this.totalCount; j++) {
+        var row = document.createElement('tr');
+        row.id = 'object' + j;
+        var properties = Object.keys(objectArray[j].getAttributes());
+        for (var i = 0; i < properties.length; i++) {
+          var col = document.createElement('td');
+          if (properties[i].localeCompare('type')) {
+            col.innerHTML = properties[i] + ' = ' + objectArray[j][properties[i]];
+          } else {
+            col.innerHTML = objectArray[j][properties[i]];
+          }
+          row.appendChild(col);
         }
-        for (var i = 0; i < object2.objectArray.length; i++) {
-          var objectListItem = document.createElement('li');
-          objectList.appendChild(objectListItem);
-          var objLink = document.createElement('a');
-          objLink.href = '#object' + (i + object1.objectArray.length);
-          objLink.innerHTML = object2.objectArray[i]['type'];
-          objectListItem.appendChild(objLink);
-          objectListItem.innerHTML += '; area = ' + object2.objectArray[i]['area']
-          + '; location = ' + object2.objectArray[i]['location'];
-        }
-        element.appendChild(objectList);
+        table.appendChild(row);
       }
     }
   }
-};
+}
+
+textInterceptor.prototype.getSummary = function(object1, object2, element) {
+  this.prevTotalCount = this.totalCount;
+  this.totalCount = object1.objectCount + object2.objectCount;
+  element.innerHTML = '';
+  element.innerHTML += this.bgColor + ' coloured canvas is ' + this.canvasDetails.width + ' by ' +
+    this.canvasDetails.height + ' of area ' + this.canvasDetails.width * this.canvasDetails.height;
+  if (this.totalCount > 1) {
+    element.innerHTML += ' Contains ' + this.totalCount + ' objects - ';
+  } else {
+    element.innerHTML += ' Contains ' + this.totalCount + ' object - ';
+  }
+
+  if (object2.objectCount > 0 || object1.objectCount > 0) {
+    totObjectTypeCount = mergeObjRecursive(object1.objectTypeCount, object2.objectTypeCount);
+    var keys = Object.keys(totObjectTypeCount);
+    for (var i = 0; i < keys.length; i++) {
+      element.innerHTML += totObjectTypeCount[keys[i]] + ' ' + keys[i] + ' ';
+    }
+
+    var objectList = document.createElement('ul');
+
+    if (this.totalCount < 100) {
+
+      object1.objectArray.forEach(function(objArrayItem){
+        var objectListItem = document.createElement('li');
+        objectList.appendChild(objectListItem);
+        var objLink = document.createElement('a');
+        objLink.href = '#object' + i;
+        objLink.innerHTML = objArrayItem['type'];
+        objectListItem.appendChild(objLink);
+        objectListItem.innerHTML += '; area = ' + objArrayItem['area'] +
+          '; location = ' + objArrayItem['location'];
+      });
+      object2.objectArray.forEach(function(objArrayItem){
+        var objectListItem = document.createElement('li');
+        objectList.appendChild(objectListItem);
+        var objLink = document.createElement('a');
+        objLink.href = '#object' + (i + object1.objectArray.length);
+        objLink.innerHTML = objArrayItem['type'];
+        objectListItem.appendChild(objLink);
+        objectListItem.innerHTML += '; area = ' + objArrayItem['area'] +
+          '; location = ' + objArrayItem['location'];
+      });
+      element.appendChild(objectList);
+    }
+  }
+}
 
 var Interceptor = new textInterceptor();
